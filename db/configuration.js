8034c920fc7a09338caee3ea216f22e43a10523d
Emiliano Pelliccioni <emilianop11@gmail.com>
2017-01-20 12:59:29 -0300

    Updating demo creation sip

--- db/configuration.js
+++ db/configuration.js
@@ -34,7 +34,7 @@ Account.findOne({email: "demo.user@did2webr.tc"}, function (err, demoAccount) {
 
 function generateDemos (index) {
   Demo.findOne({name: "demo" + index}, function (err, demo) {
-    var internalSip = process.env.VOXBONE_WEBRTC_USERNAME + "+demo" + index + "@workshop-gateway.voxbone.com";
+    var internalSip = process.env.VOXBONE_WEBRTC_USERNAME + "+demo" + index + "@sip.2webr.tc";
     var options = {
       url: 'https://click2vox.com/widget/get-id?sipuri=' + encodeURIComponent(internalSip),
       method: 'GET',
